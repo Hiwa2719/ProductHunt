428fe3024e24eff0f3968d5817190d930a714132
hiwa2719 <hiahmadyan@gmail.com>
2021-10-23 22:00:41 +0330

    added ProductCreateView test case

--- products/tests.py
+++ products/tests.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 from django.urls import reverse
 
 from . import views
+from .forms import ProductCreateForm
 from .models import Product
+
 
 #  Unit tests
 
@@ -58,8 +60,36 @@ class ProductDetailViewTestCase(TestCase):
         product = Product.objects.get(pk=2)
         response = self.client.get(reverse('products:product-detail', kwargs={'pk': 2}))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.resolver_match.fun.__name__, views.ProductDetailView.as_view().__name__)
+        self.assertEqual(response.resolver_match.func.__name__, views.ProductDetailView.as_view().__name__)
         self.assertTemplateUsed(response, 'products/product_detail.html')
         self.assertIn('product', response.context)
         self.assertContains(response, product.title)
 
+
+class ProductCreateViewTestCase(TestCase):
+    fixtures = ['fixtures/database.json']
+
+    def test_getting_product_create_page_by_anonymous_user(self):
+        response = self.client.get(reverse('products:create-product'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('login') + '?next=' + reverse('products:create-product'))
+
+    def test_getting_to_product_create_page(self):
+        """test getting create form"""
+        self.client.login(username='hiwa@gmail.com', password='asdf')
+        response = self.client.get(reverse('products:create-product'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.resolver_match.func.__name__, views.ProductCreateView.as_view().__name__)
+        self.assertTemplateUsed(response, 'products/product_form.html')
+        self.assertIn('form', response.context)
+        self.assertEqual(response.context['form'].__class__, ProductCreateForm)
+        self.assertIn('input_val', response.context)
+        self.assertContains(response, 'title')
+
+    def test_posting_product_data_to_Product_create_view(self):
+        """examining creating new product"""
+        self.client.login(username='hiwa@gmail.com', password='asdf')
+        response = self.client.post(reverse('products:create-product'), {'title': 'your cv', 'content': 'this place is hell'})
+        self.assertEqual(response.status_code, 302)
+        product = Product.objects.get(title='your cv')
+        self.assertEqual(product.content, 'this place is hell')
