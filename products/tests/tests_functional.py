0306b3f68e7b968eb116962a28d9b9e0c7387661
hiwa2719 <hiahmadyan@gmail.com>
2021-10-29 21:13:40 +0330

    changing fixture using --natural-primary --natural-foreign and also added few test case

--- products/tests/tests_functional.py
+++ products/tests/tests_functional.py
@@ -1,14 +1,18 @@
+import re
+
 from django.contrib.auth import get_user_model
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 User = get_user_model()
 
 
 class PublicFunctionalTests(StaticLiveServerTestCase):
-    fixtures = ['fixtures/database.json']
+    fixtures = ['fixtures/database2.json']
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -24,6 +28,27 @@ class PublicFunctionalTests(StaticLiveServerTestCase):
     def test_getting_login_page(self):
         """test accessing to login page"""
         self.browser.find_element_by_id('login').click()
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#id_username')))
+        self.assertEqual(self.live_server_url + reverse('login'), self.browser.current_url)
+        self.assertTrue(self.browser.find_element_by_id('id_password'))
+
+    def test_getting_signup_page(self):
+        """test accessing signup page"""
+        self.browser.find_element_by_id('signup').click()
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#id_password2')))
+        self.assertEqual(self.live_server_url + reverse('signup'), self.browser.current_url)
+        self.assertTrue(self.browser.find_element_by_id('id_password1'))
+
+    def test_clicking_on_vote(self):
+        """test clicking on vote button on index page"""
+        self.browser.find_elements_by_class_name('vote')[1].click()
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#id_username')))
+        pattern = self.live_server_url + reverse('login') + r'\?next=/products/vote/[0-9]+/'
+        match = re.search(pattern, self.browser.current_url)
+        self.assertTrue(match)
+
+    def test_getting_to_product_detail_page(self):
+        pass
 
     def tearDown(self) -> None:
         self.browser.quit()
