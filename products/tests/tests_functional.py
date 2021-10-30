d7b3ad454d26128770a03e8bbf537799ffb8c851
hiwa2719 <hiahmadyan@gmail.com>
2021-10-30 13:30:10 +0330

    functional signup test case modified for creating new user

--- products/tests/tests_functional.py
+++ products/tests/tests_functional.py
@@ -18,6 +18,7 @@ class PublicFunctionalTests(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.wait = WebDriverWait(self.browser, 10)
         self.browser.get(self.live_server_url + reverse('index'))
+        self.login_url = self.live_server_url + reverse('login')
 
     def test_getting_index_view(self):
         self.assertIn('Navbar', self.browser.page_source)
@@ -29,7 +30,7 @@ class PublicFunctionalTests(StaticLiveServerTestCase):
         """test accessing to login page"""
         self.browser.find_element_by_id('login').click()
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#id_username')))
-        self.assertEqual(self.live_server_url + reverse('login'), self.browser.current_url)
+        self.assertEqual(self.login_url, self.browser.current_url)
         self.assertTrue(self.browser.find_element_by_id('id_password'))
 
     def test_getting_signup_page(self):
@@ -37,13 +38,18 @@ class PublicFunctionalTests(StaticLiveServerTestCase):
         self.browser.find_element_by_id('signup').click()
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#id_password2')))
         self.assertEqual(self.live_server_url + reverse('signup'), self.browser.current_url)
-        self.assertTrue(self.browser.find_element_by_id('id_password1'))
+        self.browser.find_element_by_id('id_username').send_keys('George_W_bush')
+        self.browser.find_element_by_id('id_password1').send_keys('HiWa_asdf')
+        self.browser.find_element_by_id('id_password2').send_keys('HiWa_asdf')
+        self.browser.find_element_by_css_selector('input[type="submit"]').click()
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#id_username')))
+        self.assertEqual(self.browser.current_url, self.login_url)
 
     def test_clicking_on_vote(self):
         """test clicking on vote button on index page"""
         self.browser.find_elements_by_class_name('vote')[1].click()
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#id_username')))
-        pattern = self.live_server_url + reverse('login') + r'\?next=/products/vote/[0-9]+/'
+        pattern = self.login_url + r'\?next=/products/vote/[0-9]+/'
         match = re.search(pattern, self.browser.current_url)
         self.assertTrue(match)
 
